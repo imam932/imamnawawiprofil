d02c12702f792ac5b2aa72ca2da0f545917016c0
Imam <nawawu932@gmail.com>
2019-08-10 01:00:59 +0700

    push view vue

---
+++ src/views/Components.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <hero></hero>
+        <basic-elements></basic-elements>
+        <inputs></inputs>
+        <custom-controls></custom-controls>
+        <navigation></navigation>
+        <javascript-components></javascript-components>
+        <icons></icons>
+        <examples></examples>
+        <download-section></download-section>
+        <carousel></carousel>
+    </div>
+</template>
+<script>
+import Hero from "./components/Hero";
+import Carousel from "./components/Carousel";
+import Icons from "./components/Icons";
+import Examples from "./components/Examples";
+import DownloadSection from "./components/DownloadSection";
+
+export default {
+  name: "components",
+  components: {
+    Hero
+  }
+};
+</script>
